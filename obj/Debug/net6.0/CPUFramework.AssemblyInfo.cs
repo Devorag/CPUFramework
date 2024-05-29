8de9cb276ef0b4f1bd4cbb69c380d8179b03425a
Devorag <141979085+Devorag@users.noreply.github.com>
2024-05-29 19:24:08 -0400

    all other changes

--- obj/Debug/net6.0/CPUFramework.AssemblyInfo.cs
+++ obj/Debug/net6.0/CPUFramework.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CPUFramework")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cca9c7efc2c3d34b5f8c7d4a31a48ee81dad620a")]
 [assembly: System.Reflection.AssemblyProductAttribute("CPUFramework")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CPUFramework")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
