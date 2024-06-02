280c1f8341740812f3f49f2d1a59e4c0c13f5f60
Devorag <141979085+Devorag@users.noreply.github.com>
2024-06-02 16:18:59 -0400

    session 15 done in mid of session 16

--- obj/Debug/net6.0/CPUFramework.AssemblyInfo.cs
+++ obj/Debug/net6.0/CPUFramework.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CPUFramework")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+43abd8a394f272f494daf255bbbc39345757ac4b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7fcd0581bfe4da1b07da80aa91d4498dc011f4c8")]
 [assembly: System.Reflection.AssemblyProductAttribute("CPUFramework")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CPUFramework")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
